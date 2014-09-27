f2b66ec06ac0fd1cdd74d2036778127fc99c5d10
Fernando Izquierdo <fer.izquierdo@gmail.com>
2014-09-27 02:45:44 +0100

    show number of words in generated network

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,19 +1,13 @@
 class StaticController < ApplicationController
 	def network
-		render
-		@words = Word.all
-		@connections = Connection.all
-
-		# here we could generate the json file
+		# manually generate the springy-formatted json file
 		File.open(File.join(Rails.root, "data.json"), 'w') do |f|
 			f.puts "{"
-			# nodes
 			f.puts '"nodes":['
 			Word.all.each do |w|
 				f.puts w.to_generic_json
 			end
 			f.puts '],'
-			# edges
 			f.puts '"edges":['
 			Connection.all.each do |c|
 				f.puts c.to_generic_json
