bfff3fb7d64b63461b65647d5eb2348b529000a6
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-03-23 22:35:27 +0000

    added seed script and seed data as csv files

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,6 +2,10 @@ class Word < ActiveRecord::Base
 	validates :han, uniqueness: true
 	
 	def to_generic_json
-		'{label:"'+self.han+ '",meaning:"' +self.meaning+ '",pinyin:"' +self.pinyin+'"},'
+		han = self.han || ''
+		meaning = self.meaning || ''
+		pinyin = self.pinyin || ''
+		
+		'{label:"'+han+ '",meaning:"' +meaning+ '",pinyin:"' +pinyin+'"},'
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+# words
+File.open(File.join(Rails.root, "public", "words.txt")).each_line do |l|
+	han, meaning, pinyin = l.strip.split(',')
+	Word.create({han: han, meaning: meaning, pinyin: pinyin})
+end
+
+# connections (words should exist already)
+File.open(File.join(Rails.root, "public", "edges.txt")).each_line do |l|
+	from, to, color, directional = l.strip.split(',')
+	Connection.create({from: from, to: to, color: color, directional: directional})
+end
