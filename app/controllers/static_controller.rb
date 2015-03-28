f84915391b221f25a55278a26c7ae43ce902391f
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-03-28 22:55:32 +0000

    added edges to grammar points tree

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -17,18 +17,35 @@ class StaticController < ApplicationController
 		end
 	end
 	def grammar_tree
+		# Avoid representing twice the same edge
+		all_edges = []
+		all_nodes = []
+		GrammarPoint.all.each do |gp| 
+			# unique nodes
+			all_nodes << gp.to_json_node
+			# edges and possibly redundant nodes
+			edges, new_nodes = gp.to_json_connections
+			edges.each do |edge|
+				all_edges << edge unless all_edges.include?(edge)
+			end
+			new_nodes.each do |node|
+				all_nodes << node unless all_nodes.include?(node)
+			end
+		end
+
 		# manually generate the springy-formatted json file
+		# TODO DRY
 		File.open(File.join(Rails.root, "public", "data.json"), 'w') do |f|
 			f.puts "{"
 			f.puts '"nodes":['
-			GrammarPoint.all.each do |gp|
-				f.puts gp.to_generic_json
+			all_nodes.each do |n|
+				f.puts n
 			end
 			f.puts '],'
 			f.puts '"edges":['
-			#Connection.all.each do |c|
-			#	f.puts c.to_generic_json
-			#end
+			all_edges.each do |e|
+				f.puts e
+			end
 			f.puts ']'
 			f.puts "}"
 		end
--- app/models/grammar_point.rb
+++ app/models/grammar_point.rb
@@ -5,7 +5,7 @@ class GrammarPoint < ActiveRecord::Base
 	validates :pattern, presence: true
 	validates :example, presence: true
 
-	def to_generic_json
+	def to_json_node
 		pattern = sanitize(self.pattern)
 		eng = sanitize(self.eng)
 		'{eng:"'+eng+ '",label:"' +pattern+ '",example:"' +self.example+'"},'
@@ -16,7 +16,36 @@ class GrammarPoint < ActiveRecord::Base
 		path += '/' + self.h3 if self.h3
 		path
 	end
+	def to_json_connections
+		pattern = sanitize(self.pattern)
+		paths = []
+		path_nodes = []
+		if self.h2
+			h2 = sanitize(self.h2)
+			paths << json_path(h1, h2)
+			path_nodes << json_node(h1)
+			path_nodes << json_node(h2)
+			if self.h3
+				h3 = sanitize(self.h3)
+				paths << json_path(h3, pattern)
+				paths << json_path(h2, h3)
+				path_nodes << json_node(h3)
+			else
+				paths << json_path(h2, pattern)
+			end
+		else
+			paths << json_path(h1, pattern)
+		end
+		[paths, path_nodes] 
+	end
 	private
+	def json_path(from, to, directional=false)
+		'["' + from + '","' +to+ '",{color:"#00A0B0",directional:'+ directional.to_s+'}],'
+	end
+	def json_node(label, eng='', example='')
+		label = sanitize(label)
+		'{eng:"'+eng+ '",label:"' +label+ '",example:"' +self.example+'"},'
+	end
 	def sanitize(str)
 		translator = {'"' => '', 
 			          'Subject' => 'Subj.', 
