8727e2c569e8ef6f9e728d1efaaf2a89520a2551
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-04-03 00:10:57 +0100

    show list of understandable example of grammar points according to word success rate

--- app/helpers/grammar_points_helper.rb
+++ app/helpers/grammar_points_helper.rb
@@ -1,5 +1,5 @@
 module GrammarPointsHelper
-	def self.understandable_with(words, threshold = 0.4)
+	def self.understandable_with(words, threshold = 0.8)
 		# lets assume we work only with A levels
 		gps = GrammarPoint.where(level: ['A1', 'A2'])
 		understandable_gps = []
@@ -8,11 +8,11 @@ module GrammarPointsHelper
 			known_words = example_words.select{|w| words.include? w}
 			rate = known_words.size.to_f / example_words.size.to_f
 			if rate > threshold
-				understandable_gps << gp
+				understandable_gps << {gp: gp, rate: rate.to_s}
 			end
 		end
-		# return sorted?
-		understandable_gps 
+		# return sorted by the number of known words (least known first)
+		understandable_gps.sort_by{|e| e[:rate]}#.map{|e| e[:gp]}
 	end
 	private 
 	def extract_words(example)
