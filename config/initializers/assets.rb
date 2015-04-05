6a9a9bff8840e565ca2f1fb404c7c7f8d5566786
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-04-05 03:16:00 +0100

    undo config and updated css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( springy.js springyui.js jquery.min.js .jpg)
+Rails.application.config.assets.precompile += %w( springy.js springyui.js jquery.min.js dragon_3.jpg)
