70b5987d5f8d3b6e591166742320811bc45cc690
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-04-05 22:54:02 +0100

    added example of testing with Nokogiri directly

--- test/integration/home_flow_test.rb
+++ test/integration/home_flow_test.rb
@@ -1,4 +1,6 @@
 require 'test_helper'
+#require 'nokogiri'   
+#require 'open-uri'
 
 class HomeFlowTest < ActionDispatch::IntegrationTest
    test "Go to the home page and find a wellcome msg" do
@@ -9,4 +11,9 @@ class HomeFlowTest < ActionDispatch::IntegrationTest
 		 assert_select 'footer', {count: 1}
 		 assert_select 'header', {count: 1}
    end
+	 test "Check the content directly with Nokogiri" do
+		 page = Nokogiri::HTML(open('http://0.0.0.0:3000/'))
+		 assert page.css("div#main-container").css("h1").text.include?("Let's learn Mandarin")
+	 end
+
 end
