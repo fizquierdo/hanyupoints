b4ee07ea0332d8c89d642b4d13f0dddf2ce8b817
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-04-26 00:53:08 +0100

    changed title

--- test/integration/home_flow_test.rb
+++ test/integration/home_flow_test.rb
@@ -6,7 +6,7 @@ class HomeFlowTest < ActionDispatch::IntegrationTest
    test "Go to the home page and find a wellcome msg" do
 		 get '/'
 		 assert_response :success
-		 assert_select 'title', 'Wodehanyu'
+		 assert_select 'title', 'Hanyu point by point'
 		 assert_select 'footer', {count: 1}
 		 assert_select 'header', {count: 1}
    end
